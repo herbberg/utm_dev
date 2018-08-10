43bb2ed62d54471c2389c394734317a40c6914bc
Bernhard Arnold <bernhard.arnold@cern.ch>
2018-08-10 16:02:41 +0200

    reverted accedential changes to tmUtil.hh/.cc

--- tmUtil/tmUtil.hh
+++ tmUtil/tmUtil.hh
@@ -1,14 +1,32 @@
-#ifndef tmutil_tmUtil_hh
-#define tmutil_tmUtil_hh
+/**
+ * @author Bernhard Arnold
+ * @author Takashi Matsushita
+ * @date: 14 Feb 2005
+ */
 
+#ifndef tmUtil_hh
+#define tmUtil_hh
+/*====================================================================*
+ * declarations
+ *====================================================================*/
+/*--------------------------------------------------------------------*
+ * headers
+ *--------------------------------------------------------------------*/
 #include "tmUtil/macros.hh"
 
 #include <regex.h>
 
-#include <sstream>
 #include <string>
 #include <vector>
 
+/*--------------------------------------------------------------------*
+ * constants
+ *--------------------------------------------------------------------*/
+/* nope */
+
+/*--------------------------------------------------------------------*
+ * function
+ *--------------------------------------------------------------------*/
 namespace tmutil {
 
 /**
@@ -50,57 +68,6 @@ int regex_match(regex_t* regex,
                 const std::string& text,
                 std::vector<std::string>& tokens);
 
-/** Returns environment variable.
-  * Returns empty string if environment variable is not set.
-  */
-std::string getEnviron(const std::string& name);
-
-/** Returns environment variable.
-  * Returns fallback value if environment variable is not set.
-  */
-std::string getEnviron(const std::string& name, const std::string& fallback);
-
-// PROTOTYPE
-template<typename T>
-std::string quote(const T& literal)
-{
-  constexpr char quote = '\'';
-  std::ostringstream oss;
-  oss << quote << literal << quote;
-  return oss.str();
-}
-
-// PROTOTYPE
-template<typename T>
-std::string join(const T& iterable, const std::string& delim)
-{
-  std::ostringstream oss;
-  for (const auto& value : iterable)
-    oss << (oss.tellp() ? delim : std::string()) << value;
-  return oss.str();
-}
-
-// PROTOTYPE
-template<typename T>
-std::string join_quoted(const T& iterable, const std::string& delim)
-{
-  std::ostringstream oss;
-  for (const auto& value : iterable)
-    oss << (oss.tellp() ? delim : std::string()) << quote(value);
-  return oss.str();
-}
-
-// PROTOTYPE
-template<typename T>
-std::string format_parser_error(const T& begin, const T& end, const T& pos)
-{
-  std::ostringstream oss;
-  size_t n = std::distance(begin, pos);
-  oss << std::string(begin, end) << std::endl;
-  oss << std::string(n, ' ') << '^' << std::endl;
-  return oss.str();
-}
-
 /** Version number container.
   *
   * Version v("1.2.3");
@@ -113,8 +80,8 @@ struct Version
   void str(const std::string& version);
   std::string str() const;
 
-  typedef std::vector<unsigned> value_type;
-  value_type values;
+  typedef std::vector<unsigned> data_t;
+  data_t data;
 };
 
 /* Version number comparision. */
@@ -127,4 +94,5 @@ bool operator>=(const Version& lhs, const Version& rhs);
 
 } // namespace tmutil
 
-#endif // tmutil_tmUtil_hh
+#endif // tmUtil_hh
+/* eof */
