e756547e879e33c8ffe85c7af2fee52af2f821ef
Bernhard Arnold <bernhard.arnold@cern.ch>
2018-08-14 13:51:35 +0200

    extened tmEventSetup unit tests

--- tmEventSetup/test/unittest.cc
+++ tmEventSetup/test/unittest.cc
@@ -1,8 +1,12 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE tmEventSetup
 
-#include "tmUtil/tmUtil.hh"
 #include "tmEventSetup/tmEventSetup.hh"
+
+#include "tmEventSetup/esAlgorithmHandle.hh"
+#include "tmEventSetup/esAlgorithm.hh"
+
+#include "tmUtil/tmUtil.hh"
 
 #include <boost/test/unit_test.hpp>
 
@@ -17,6 +21,101 @@ namespace tmeventsetup
   extern long long toFixedPoint(double, const size_t);
 }
 
+BOOST_AUTO_TEST_SUITE(esTriggerMenu)
+
+BOOST_AUTO_TEST_CASE(esAlgorithmHandle_init)
+{
+  tmtable::Row row;
+  row["name"] = "L1_Unnamed";
+  row["index"] = "42";
+  row["module_id"] = "0";
+  row["module_index"] = "1";
+  row["expression"] = "mass{MU4,MU2}[MASS_Z] OR TAU42";
+
+  tmeventsetup::esAlgorithmHandle handle;
+  handle.init(row);
+
+  tmeventsetup::esAlgorithm* algorithm = dynamic_cast<tmeventsetup::esAlgorithm*>(&handle);
+
+  BOOST_CHECK_EQUAL("L1_Unnamed", algorithm->getName());
+  BOOST_CHECK_EQUAL(42, algorithm->getIndex());
+  BOOST_CHECK_EQUAL(0, algorithm->getModuleId());
+  BOOST_CHECK_EQUAL(1, algorithm->getModuleIndex());
+  BOOST_CHECK_EQUAL("mass{MU4,MU2}[MASS_Z] OR TAU42", algorithm->getExpression());
+  BOOST_CHECK_EQUAL("", algorithm->getExpressionInCondition());
+}
+
+BOOST_AUTO_TEST_CASE(esAlgorithmHandle_setRpnVector)
+{
+  tmtable::Row row;
+  row["name"] = "L1_setRpnVector";
+  row["index"] = "42";
+  row["module_id"] = "0";
+  row["module_index"] = "1";
+  row["expression"] = "MU7 AND TAU3";
+
+  std::map<std::string, std::string> conditionMap;
+  conditionMap["MU7"] = "SingleMu_4";
+  conditionMap["TAU3"] = "SingleTau_5";
+
+  tmeventsetup::esAlgorithmHandle handle;
+  handle.init(row);
+  handle.setExpressionInCondition(conditionMap);
+
+  tmeventsetup::esAlgorithm* algorithm = dynamic_cast<tmeventsetup::esAlgorithm*>(&handle);
+
+  BOOST_CHECK_EQUAL("L1_setRpnVector", algorithm->getName());
+  BOOST_CHECK_EQUAL(42, algorithm->getIndex());
+  BOOST_CHECK_EQUAL(0, algorithm->getModuleId());
+  BOOST_CHECK_EQUAL(1, algorithm->getModuleIndex());
+  BOOST_CHECK_EQUAL("MU7 AND TAU3", algorithm->getExpression());
+  BOOST_CHECK_EQUAL("SingleMu_4 AND SingleTau_5", algorithm->getExpressionInCondition());
+}
+
+BOOST_AUTO_TEST_CASE(esAlgorithmHandle_setExpressionInCondition)
+{
+  tmtable::Row row;
+  row["name"] = "L1_setExpressionInCondition";
+  row["index"] = "42";
+  row["module_id"] = "0";
+  row["module_index"] = "1";
+  row["expression"] = "mass{MU4,MU2}[MASS_Z] OR TAU42 AND EXT_SIG_0";
+
+  std::map<std::string, std::string> conditionMap;
+  conditionMap["mass{MU4,MU2}[MASS_Z]"] = "mass_123";
+  conditionMap["TAU42"] = "single_tau_124";
+  conditionMap["EXT_SIG_0"] = "ext_signal_125";
+
+  tmeventsetup::esAlgorithmHandle handle;
+  handle.init(row);
+  handle.setExpressionInCondition(conditionMap);
+
+  // Isn't this all a quite a big rubish?!
+  tmeventsetup::esTriggerMenuHandle triggerMenuHandle;
+  handle.setRpnVector(triggerMenuHandle.parse(handle.getExpressionInCondition()));
+
+  tmeventsetup::esAlgorithm* algorithm = dynamic_cast<tmeventsetup::esAlgorithm*>(&handle);
+
+  BOOST_CHECK_EQUAL("L1_setExpressionInCondition", algorithm->getName());
+  BOOST_CHECK_EQUAL(42, algorithm->getIndex());
+  BOOST_CHECK_EQUAL(0, algorithm->getModuleId());
+  BOOST_CHECK_EQUAL(1, algorithm->getModuleIndex());
+  BOOST_CHECK_EQUAL("mass{MU4,MU2}[MASS_Z] OR TAU42 AND EXT_SIG_0", algorithm->getExpression());
+  BOOST_CHECK_EQUAL("mass_123 OR single_tau_124 AND ext_signal_125", algorithm->getExpressionInCondition());
+
+  std::vector<std::string> rpn_vector_ref;
+  rpn_vector_ref.push_back("mass_123");
+  rpn_vector_ref.push_back("single_tau_124");
+  rpn_vector_ref.push_back("OR");
+  rpn_vector_ref.push_back("ext_signal_125");
+  rpn_vector_ref.push_back("AND");
+  std::vector<std::string> rpn_vector = algorithm->getRpnVector();
+
+  BOOST_CHECK_EQUAL_COLLECTIONS(rpn_vector.begin(), rpn_vector.end(),
+                                rpn_vector_ref.begin(), rpn_vector_ref.end());
+}
+
+BOOST_AUTO_TEST_SUITE_END()
 
 BOOST_AUTO_TEST_SUITE(esTriggerMenu)
 
