761468f89ef11b50357c527349625020038aa96e
Bernhard Arnold <bernhard.arnold@cern.ch>
2020-01-08 12:22:33 +0100

    updated versions #12

--- tmEventSetup/version.h
+++ tmEventSetup/version.h
@@ -1,3 +1,3 @@
 #define tmEventSetup_VERSION_MAJOR 0
 #define tmEventSetup_VERSION_MINOR 7
-#define tmEventSetup_VERSION_PATCH 3
+#define tmEventSetup_VERSION_PATCH 4
--- tmTable/version.h
+++ tmTable/version.h
@@ -1,3 +1,3 @@
 #define tmTable_VERSION_MAJOR 0
 #define tmTable_VERSION_MINOR 7
-#define tmTable_VERSION_PATCH 3
+#define tmTable_VERSION_PATCH 4
