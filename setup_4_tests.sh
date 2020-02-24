419d7530eb7bc82e2f076698143ba4af5110895b
Herbert Bergauer <herbert.bergauer@oeaw.ac.at>
2020-02-24 16:09:37 +0100

    added script

---
+++ setup_4_tests.sh
@@ -0,0 +1,18 @@
+UTM_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Library paths
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${UTM_ROOT}/tmUtil
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${UTM_ROOT}/tmGrammar
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${UTM_ROOT}/tmTable
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${UTM_ROOT}/tmXsd
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${UTM_ROOT}/tmEventSetup
+
+# # Python package/module paths
+# export PYTHONPATH=${PYTHONPATH}:${UTM_ROOT}/tmTable
+# export PYTHONPATH=${PYTHONPATH}:${UTM_ROOT}/tmGrammar
+# export PYTHONPATH=${PYTHONPATH}:${UTM_ROOT}/tmEventSetup
+
+# Environmentals
+export UTM_ROOT=${UTM_ROOT}
+export UTM_XSD_DIR=${UTM_ROOT}/tmXsd
+export LC_ALL="en_US.UTF-8" # set locale required by xsd command
